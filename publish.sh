1fd3dac34af0b206e72c2f98a37595585a5628a4
eat-sleep-code <info@eat-sleep-code.com>
2024-07-22 22:46:28 -0700

    v2024.07.22.224612: Remove grunt and babel, add webpack

---
+++ publish.sh
@@ -0,0 +1,50 @@
+commitMessage="$1"
+release="$2"
+version=$(date +'v%Y.%m.%d.%H%M%S')
+
+
+echo -e '\033[0;32m Getting latest from "develop" branch...\e[0m'
+git pull origin develop
+
+
+echo -e '\033[0;32m Installing NPM packages...\e[0m'
+npm install --verbose
+
+
+echo -e '\033[0;32m Running NPM scripts...\e[0m'
+npm run scss
+npm run pack
+npm run obfuscate
+
+
+#echo -e '\033[0;32m Copying audio...\e[0m'
+#rm -Rf dist/audio
+#mkdir -p dist/audio
+#cp -r src/audio/*.* dist/audio/
+
+
+#echo -e '\033[0;32m Copying images...\e[0m'
+#rm -Rf dist/images
+#mkdir -p dist/images
+#cp -r src/images/*.* dist/images/
+
+
+# If enabling add ?v={{version}} to any URLs you want to bust
+#echo -e '\033[0;32m Adding cache-busting tag...\e[0m'
+#grep -rl '{{version}}' ./public/*.html | xargs sed -i "s/{{version}}/$(date '+%Y%m%d%H%M%S')/g"
+
+
+echo -e '\033[0;32m Committing changes to "develop" branch...\e[0m'
+git add . -A
+git commit -m "$version: $commitMessage"
+git push -u origin develop
+
+# If a release...
+if [ "$release" == "release" ]; then
+    echo -e '\033[0;32m Pushing release to "master/main" branch and deploying...\e[0m'
+    git push origin develop:master --force
+    git tag -a "$version" -m "$version: $commitMessage"
+    git push -u origin "$version"
+    firebase login --reauth
+    firebase deploy
+fi
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+	mode: 'production',
+	entry: {
+		'invaders': [
+			'./src/js/audio.js',
+            './src/js/engine.js',
+            './src/js/game.js',
+            './src/js/analytics.js'
+        ]
+	},
+	output: {
+		filename: '[name].min.js',
+		path: __dirname + '/src/js/',
+		hashFunction: 'xxhash64'
+	},
+	optimization: {
+		mangleExports: false,
+		minimize: true,
+		minimizer: [
+			new TerserPlugin({
+				terserOptions: {
+					format: {
+						comments: false
+					}
+				},
+				extractComments: false
+			})
+		]
+	}
+  };
